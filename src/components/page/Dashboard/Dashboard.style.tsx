41ed5ccad262e8ab316cabfa174617d1d333eb8e
Papichaya-Dev <lo0kpad@hotmail.com>
2022-05-12 03:02:58 +0700

    :art:[completed] Landing page & Login UI :art:

--- src/components/page/Dashboard/Dashboard.style.tsx
+++ src/components/page/Dashboard/Dashboard.style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import background_dashboard from "../../../shared/images/background_dashboard.png";
-import { Button } from "antd";
+import { Button, Input, Modal } from "antd";
 export const Background = styled.div`
   background-image: url(${background_dashboard});
   background-repeat: no-repeat;
@@ -33,4 +33,58 @@ export const Button_Login = styled(Button)`
   font-size: 24px;
   border-radius: 35px;
   border: none;
+`;
+
+export const Modal_Login = styled(Modal)`
+  & .ant-modal-footer {
+    border: none;
+  }
+  & .ant-modal-header {
+    border: none;
+    border-radius: 30px !important;
+  }
+  & .ant-modal-title {
+    text-align: center;
+    font-weight: bolder;
+    font-size: 35px;
+    padding: 24px;
+  }
+  & .ant-modal-content {
+    border-radius: 30px !important;
+  }
+`;
+
+export const Input_Email = styled(Input)`
+  margin: 0;
+  border-radius: 30px;
+  margin-bottom: 30px;
+  height: 50px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+`;
+
+export const Input_Password = styled(Input.Password)`
+  margin: 0;
+  border-radius: 30px;
+  height: 50px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+  margin-bottom: 10px;
+`;
+
+export const Forget_Password = styled.div`
+  color: #a2b4fa;
+  text-align: right;
+  font-size: 16px;
+`;
+
+export const Button_Login_Form = styled(Button)`
+  border-radius: 30px;
+  background-color: #eb5f91;
+  color: white;
+  height: 50px;
+  font-size: 16px;
+  margin: 0 auto;
+  display: block;
+  width: 213px;
 `;
--- src/components/page/Dashboard/Dashboard.tsx
+++ src/components/page/Dashboard/Dashboard.tsx
@@ -1,15 +1,38 @@
-import React from "react";
-import { Background, Button_Login, Text_Header, Text_Login } from "./Dashboard.style";
+import React, { useState } from "react";
+import { Background, Button_Login, Button_Login_Form, Forget_Password, Input_Email, Input_Password, Modal_Login, Text_Header, Text_Login } from "./Dashboard.style";
 import { useHistory } from "react-router-dom";
+import { LockFilled, MailFilled } from "@ant-design/icons";
+
 function Dashboard() {
   const history = useHistory();
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div>
       <Background>
         <Text_Header>กล่องยายินดีต้อนรับ</Text_Header>
         <Text_Login>กรุณาเข้าสู่ระบบเพื่อใช้งาน</Text_Login>
-        <Button_Login onClick={() => history.push("/home")}>เข้าสู่ระบบ</Button_Login>
+        <Button_Login onClick={showModal}>เข้าสู่ระบบ</Button_Login>
+        <Modal_Login
+          title="เข้าสู่ระบบ"
+          visible={isModalVisible}
+          onCancel={handleCancel}
+          closable={false}
+          cancelButtonProps={{ style: { display: "none" } }}
+          okButtonProps={{ style: { display: "none" } }}
+        >
+          <Input_Email placeholder="E-mail" size="large" prefix={<MailFilled />} />
+          <Input_Password placeholder="Password" size="large" prefix={<LockFilled />} />
+          <Forget_Password>ลืมรหัสผ่าน ?</Forget_Password>
+          <Button_Login_Form onClick={() => history.push("/home")}>เข้าสู่ระบบ</Button_Login_Form>
+        </Modal_Login>
       </Background>
     </div>
   );
